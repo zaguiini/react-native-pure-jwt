116ab40d0c957ca00646f0d874e8eb34949ab8e3
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2018-01-30 21:15:50 -0200

    Update RNJwtModule.java

--- android/src/main/java/com/zaguini/rnjwt/RNJwtModule.java
+++ android/src/main/java/com/zaguini/rnjwt/RNJwtModule.java
@@ -24,7 +24,6 @@ public class RNJwtModule extends ReactContextBaseJavaModule {
     return builder.create();
   }
 
-
   private String[] supportedAlgorithms = {"HS256"};
 
   public RNJwtModule(ReactApplicationContext reactContext) {
