8adb28a3fe0446457169c295c40ebcf3faf2aa10
Luis Felipe Zaguini <luis.ferreira@br.experian.com>
2018-01-30 01:56:40 -0200

    added build.gradle

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { NativeModules } from 'react-native'
+import b64 from 'base-64'
+
 const jwt = NativeModules.RNJwtAndroid
 
 const API = {
   sign(claims, secret) {
-    return jwt.sign(claims, btoa(secret))
+    return jwt.sign(claims, b64.encode(secret))
   }
 }
 
