b08803f4762c9c61f60efc3ec383d245e5d9be25
Luis Felipe Zaguini <luis.ferreira@br.experian.com>
2018-01-30 00:54:26 -0200

    added android files

---
+++ android/src/main/java/com/zaguini/rnjwt/RNJwtModule.java
@@ -0,0 +1,83 @@
+package com.zaguini.rnjwt;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableMap;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.CompressionCodecs;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Iterator;
+
+public class RNJwtModule extends ReactContextBaseJavaModule {
+  private String[] supportedAlgorithms = {"HS256", "teste"};
+
+  public RNJwtModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public String getName() {
+    return "RNJwtAndroid";
+  }
+
+  // TODO: decode method
+  // TODO: verify method
+  // TODO: error checking
+  // Source: https://github.com/auth0/node-jsonwebtoken
+
+  @ReactMethod
+  public void sign(ReadableMap bruteClaims, String base64Secret, Promise callback) {
+    HashMap<String, Object> claims = bruteClaims.toHashMap();
+    String alg = "HS256";
+
+    JwtBuilder constructedToken = Jwts.builder();
+
+    if(!claims.containsKey("exp") || claims.get("exp") == null) {
+      callback.reject("you must pass the expiration Date (exp)");
+
+      return;
+    }
+
+    if(claims.containsKey("alg") && claims.get("alg") != null) {
+      String tmpAlg = claims.get("alg").toString();
+
+      if(Arrays.asList(this.supportedAlgorithms).contains(tmpAlg)) {
+        alg = tmpAlg;
+      }
+    }
+
+    Iterator it = claims.entrySet().iterator();
+
+    while(it.hasNext()) {
+      Map.Entry pair = (Map.Entry) it.next();
+
+      Object key = pair.getKey();
+      Object value = pair.getValue();
+
+      if(new String("alg").equals(key)) {
+        continue;
+      }
+
+      constructedToken.claim(key.toString(), value);
+      it.remove();
+    }
+
+    // TODO: change algorithm based on `alg` variable
+    constructedToken = constructedToken.signWith(SignatureAlgorithm.HS256, base64Secret);
+
+    String token = constructedToken.compact();
+
+    callback.resolve(token);
+  }
+}
