20d2f59bc2d2f77dab29f1e4af5ddae0c961918c
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2018-01-30 18:58:52 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 import { NativeModules } from 'react-native'
-import b64 from 'base-64'
 
 const jwt = NativeModules.RNJwtAndroid
 
-const API = {
-  sign(claims, secret) {
-    return jwt.sign(claims, b64.encode(secret))
-  }
-}
-
-export default API
+export default jwt
